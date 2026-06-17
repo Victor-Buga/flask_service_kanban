ffd231a93e24d7796a725c51dd2be2725ef1a619
amine_elaidous <amine.elaidous@icloud.com>
2026-06-17 21:39:12 +0200

    test(s2): ajout test client HTML/JS et activation CORS

--- service2_statistiques/app.py
+++ service2_statistiques/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import numpy as np
 from scipy import stats
 
 app = Flask(__name__)
+CORS(app)
 
 
 def validate_data(data, key='data'):
